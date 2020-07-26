30972ddd58d391a3143d59bf4cbe5411d5d24170
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2020-07-26 03:54:21 -0300

    add discrição as imgs do owl carousel

--- js/main_js.js
+++ js/main_js.js
@@ -1,5 +1,37 @@
 //-----------------------------OWL CAROUSEL---------------------------//
 
+
+$(document).ready(function(){
+
+    $(".owl-carousel").owlCarousel({
+        items: 4,
+        loop: true,
+        margin: 20,
+        nav: true,
+        navText: ["Anterior", "Próximo"],
+        autoplay: true,
+        autoplayTimeout: 3000,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            360: {
+                items: 1
+            },
+            520:{
+                items: 3
+            },
+            1000: {
+                items: 4
+            }
+            
+        }
+        
+    });
+
+});
+
 /*$(document).ready(function() {
  
     $("#owl-demo").owlCarousel({
