390de2d29d3b153ac827a0bb54407e00921c71c4
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2020-07-27 01:18:43 -0300

    mais @media pro carousel caption até 411px

--- js/main_js.js
+++ js/main_js.js
@@ -32,22 +32,6 @@ $(document).ready(function(){
 
 });
 
-/*$(document).ready(function() {
- 
-    $("#owl-demo").owlCarousel({
-   
-        autoPlay: 3000, //Set AutoPlay to 3 seconds
-   
-        items : 4,
-        itemsDesktop : [1199,3],
-        itemsDesktopSmall : [979,3]
-   
-    });
-   
-});*/
-
-
-
 /**********************************************FORM DE CONTATO*****************************************/
 
 function Enviar() {
